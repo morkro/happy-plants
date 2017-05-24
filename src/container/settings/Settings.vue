193c8940e0f67d5e76e76a3851eee12ee575381d
Moritz Kröger <write@morkro.de>
2017-05-24 21:03:10 +0200

    Added basic data download in settings page

--- src/container/settings/Settings.vue
+++ src/container/settings/Settings.vue
@@ -1,12 +1,14 @@
 <template>
-  <main>
+  <main class="main-wireframe">
     <app-header :back="true">
       <h1 slot="title">Settings</h1>
     </app-header>
 
     <section>
-      export data<br />
-      show scientific name instead<br />
+      <div class="settings-data">
+        <button @click="downloadData">download data</button>
+        <button @click="importData">import data</button>
+      </div>
     </section>
   </main>
 </template>
@@ -18,6 +20,22 @@
     name: 'Settings',
     components: {
       'app-header': AppHeader
+    },
+    methods: {
+      downloadData () {
+        // TODO: Actually trigger download.
+        this.$localforage.keys()
+          .then(keys =>
+            Promise.all(keys.map(k => this.$localforage.getItem(k))))
+          .then(JSON.stringify)
+          .then(console.log)
+      },
+      importData () {
+        // TODO: Implement import logic with `<input type="file" />`
+        console.log('import')
+      }
     }
   }
 </script>
+
+<style lang="scss" scoped></style>
