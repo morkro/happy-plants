ae2a1d9de287a4f64f905ed256b55ba77fb70e23
Moritz Kröger <write@morkro.de>
2017-10-22 20:49:48 +0200

    Added rel='noopener' to outgoing links

--- src/app/settings/components/SettingsAbout.vue
+++ src/app/settings/components/SettingsAbout.vue
@@ -3,9 +3,9 @@
     <p>
       Happy Plants is a progressive web application
       build with Vue.js and other great web technologies by
-      <a href="http://moritz.berlin" target="_blank">Moritz Kröger</a>.
+      <a href="http://moritz.berlin" target="_blank" rel="noopener">Moritz Kröger</a>.
       <br /><br />
-      The codebase is open source and availabe on <a href="https://github.com/morkro/happy-plants" target="_blank">GitHub</a>.
+      The codebase is open source and availabe on <a href="https://github.com/morkro/happy-plants" target="_blank" rel="noopener">GitHub</a>.
       Any contribution is very welcome!
     </p>
   </section>
