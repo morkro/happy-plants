b43f737dbff0fca9914589145eba4c098d9001dc
Moritz Kröger <write@morkro.de>
2017-11-15 21:34:09 +0100

    :zap: (files) Resize photo upload to device width for better performance

--- src/utils/blob.js
+++ src/utils/blob.js
@@ -1,4 +1,5 @@
 import blobUtil from 'blob-util'
+import hermitResize from './hermiteResize'
 
 export const isBase64 = string => {
   try {
@@ -7,6 +8,11 @@ export const isBase64 = string => {
     return false
   }
 }
+
+export const hasReaderSupport = (
+  typeof FileReader !== 'undefined' ||
+  typeof URL !== 'undefined'
+)
 
 export const isBlobbable = blob => (
   blob instanceof Blob ||
@@ -28,4 +34,39 @@ export function convertToBlob (config) {
 
   return blobUtil.base64StringToBlob(config.blob)
     .then(blob => Object.assign({}, config, { blob }))
+}
+
+export function resizeBlob (file, options = {}) {
+  return new Promise((resolve, reject) => {
+    if (!isBlobbable(file) && !hasReaderSupport) {
+      reject(new Error('File is not a blob'))
+    }
+
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+    const img = new Image()
+
+    img.src = blobUtil.createObjectURL(file)
+    img.onload = () => {
+      const width = img.width
+      const height = img.height
+      const resizedWidth = options.width ? Math.ceil(options.width) : width / 2
+      const resizedHeight = Math.ceil(height * resizedWidth / width)
+
+      canvas.width = width
+      canvas.height = height
+
+      ctx.drawImage(img, 0, 0)
+
+      blobUtil.canvasToBlob(hermitResize(canvas, width, height, resizedWidth, resizedHeight))
+        .then(blob => {
+          blobUtil.revokeObjectURL(img.src)
+          resolve(blob)
+        })
+        .catch(error => {
+          blobUtil.revokeObjectURL(img.src)
+          reject(error)
+        })
+    }
+  })
 }
