48cb6d17a361ec76666c13b0fb1d86329092e79c
Moritz Kröger <write@morkro.de>
2017-11-30 23:44:43 +0100

    :bug: (build) Fixes accidental chalk method renamings

--- build/build.js
+++ build/build.js
@@ -29,7 +29,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }) + '\n\n')
 
     console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.var(--yellow)(
+    console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
     ))
