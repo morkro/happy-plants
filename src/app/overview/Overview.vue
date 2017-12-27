208117fcc63b7d0761ee53df9d5db89945850f0a
Moritz Kröger <write@morkro.de>
2017-12-27 20:56:53 +0100

    :sparkles: (overview) Tapping app header now scrolls to top

--- src/app/overview/Overview.vue
+++ src/app/overview/Overview.vue
@@ -1,7 +1,9 @@
 <template>
   <main class="main-wireframe">
     <app-header :settings="true">
-      <h1 slot="title">Happy Plants</h1>
+      <h1 slot="title" @click="scrollTop">
+        Happy Plants
+      </h1>
     </app-header>
 
     <!-- Alert window pops up as confirmation the user is about to delete plants. -->
@@ -208,6 +210,25 @@
       ]),
       reset () {
         Object.assign(this.$data, this.$options.data()) // Reset state
+      },
+      scrollTop () {
+        const duration = 222
+        const cosParameter = window.scrollY / 2
+        let count = 0
+        let oldTimestamp = performance.now()
+
+        function step (newTimestamp) {
+          count += Math.PI / (duration / (newTimestamp - oldTimestamp))
+
+          if (count >= Math.PI) window.scrollTo(0, 0)
+          if (window.scrollY === 0) return
+
+          window.scrollTo(0, Math.round(cosParameter + cosParameter * Math.cos(count)))
+          oldTimestamp = newTimestamp
+          window.requestAnimationFrame(step)
+        }
+
+        window.requestAnimationFrame(step)
       },
       toggleDeleteSelection (item) {
         if (item.selected) {
