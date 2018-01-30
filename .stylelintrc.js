97f781dbde54428ffe2db9c6094f1aaea4ed2d53
Moritz Kröger <write@morkro.de>
2018-01-30 22:13:54 +0100

    :art: Double quotes to single quotes

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-  "processors": ["stylelint-processor-html"],
-  "extends": "stylelint-config-standard",
-  "rules": {
-    "at-rule-no-unknown": [true, {
-      "ignoreAtRules": [
-        "function",
-        "if",
-        "extends",
-        "warn",
-        "return",
-        "for"
+  'processors': ['stylelint-processor-html'],
+  'extends': 'stylelint-config-standard',
+  'rules': {
+    'at-rule-no-unknown': [true, {
+      'ignoreAtRules': [
+        'function',
+        'if',
+        'extends',
+        'warn',
+        'return',
+        'for'
       ]
     }],
-    "no-empty-source": null,
-    "declaration-empty-line-before": null,
-    "custom-property-empty-line-before": null
+    'no-empty-source': null,
+    'declaration-empty-line-before': null,
+    'custom-property-empty-line-before': null
   }
 }
