ef764173b4bfa0a1b7fd3644d0ceb88bd8c05d48
Moritz Kröger <write@morkro.de>
2018-01-31 18:40:56 +0100

    :wrench: (webpack) Updated webpack config with latest upstream changes

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,6 +2,9 @@
 
 module.exports = {
   'plugins': {
+    'postcss-import': {},
+    'postcss-url': {},
+    'postcss-custom-media': {},
     // to edit target browsers: use 'browserlist' field in package.json
     'autoprefixer': {}
   }
