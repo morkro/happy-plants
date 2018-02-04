9633b6fa98c7d887ce3cfbf2649786ef9e951b01
Moritz Kröger <write@morkro.de>
2018-02-04 13:13:10 +0100

    :speech_balloon: (about) Updated about page

--- src/app/settings/components/SettingsAbout.vue
+++ src/app/settings/components/SettingsAbout.vue
@@ -1,14 +1,28 @@
 <template>
   <section>
     <p>
-      HappyPlants is a progressive web application
-      build with Vue.js and other great web technologies by
-      <a href="http://moritz.berlin" target="_blank" rel="noopener">Moritz Kröger</a>.
+      <strong>HappyPlants</strong> is all about organising,
+      collecting all sorts of information and trivia,
+      and adding notes to your little friends. Basically, creating your own plant database.
       <br /><br />
-      The codebase is open source and availabe on <a href="https://github.com/morkro/happy-plants" target="_blank" rel="noopener">GitHub</a>.
-      Any contribution is very welcome!
+      This app is a <em>progressive web application</em>
+      build with Vue.js and other great web technologies by
+      <a href="http://moritz.berlin" target="_blank" rel="noopener"><strong>Moritz Kröger</strong></a>.
+      <br /><br />
+      The codebase is open source and availabe on
+      <a href="https://github.com/morkro/happy-plants" target="_blank" rel="noopener">
+        <feather-github />GitHub
+      </a>.
     </p>
-    <hr />
+
+    <div class="contrubtion-section">
+      <em>
+        Bugs? Feature requests? Any other issues?
+        Please reach out to me (Email, GitHub, ...).<br />
+        <feather-heart /> Any contribution is very welcome!
+      </em>
+    </div>
+
     <h2>Credits</h2>
     <p>
       Plant icons made by
@@ -17,24 +31,70 @@
       </a>,
       licensed under CC 3.0.
     </p>
+    <hr />
+
+    <h2>License</h2>
+    <a href="https://github.com/morkro/happy-plants/blob/master/LICENSE" target="_blank" rel="noopener">
+      MIT
+    </a>
   </section>
 </template>
 
 <script>
   export default {
-    name: 'SettingsAbout'
+    name: 'SettingsAbout',
+
+    components: {
+      'feather-github': () =>
+        import('vue-feather-icon/components/github' /* webpackChunkName: "settings" */),
+      'feather-heart': () =>
+        import('vue-feather-icon/components/heart' /* webpackChunkName: "settings" */)
+    }
   }
 </script>
 
 <style lang="postcss" scoped>
+  @import "../../../styles/media-queries";
+
   section {
     padding: var(--base-gap);
     line-height: 150%;
+
+    & h2 {
+      margin-bottom: var(--base-gap);
+    }
 
     & hr {
       margin: var(--base-gap) 0;
       border: none;
       border-top: 3px solid var(--transparency-black-light);
+    }
+
+    & svg {
+      width: var(--icon-size-small);
+      height: var(--icon-size-small);
+      vertical-align: middle;
+      margin-right: calc(var(--base-gap) / 3);
+    }
+  }
+
+  .contrubtion-section {
+    background: var(--background-primary);
+    margin: var(--base-gap) 0;
+    padding: var(--base-gap);
+    width: 100vw;
+    transform: translateX(calc(-1 * var(--base-gap)));
+    font-size: var(--text-size-small);
+
+    @media (--min-desktop-viewport) {
+      width: 100%;
+      transform: none;
+    }
+
+    & svg {
+      stroke: var(--brand-red);
+      margin-right: 0;
+      transform: translateY(-2px);
     }
   }
 </style>
