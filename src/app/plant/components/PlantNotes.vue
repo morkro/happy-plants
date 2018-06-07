5d9a875882fd1bd676a3ca0a245ad26b9fb4164b
Moritz Kröger <write@morkro.de>
2018-06-08 00:02:36 +0200

    :children_crossing: (plant) Updates layout of notes

--- src/app/plant/components/PlantNotes.vue
+++ src/app/plant/components/PlantNotes.vue
@@ -23,12 +23,28 @@
         </button>
       </app-dialog>
 
-      <div v-if="!content" key="notes-empty">
+      <div v-if="!content"
+        key="notes-empty"
+        class="notes-empty">
         <p>Seems like you haven't added any notes yet.</p>
         <button @click="toggleNotes">Add notes</button>
       </div>
-      <div v-else key="notes-available">
-        <button @click="toggleNotes">Show notes</button>
+
+      <div v-else
+        key="notes-available"
+        :class="{ 'notes-available': true, 'show-all': showAllNotes }">
+        <div class="notes-preview">
+          <p>{{ content }}</p>
+        </div>
+        <div class="notes-actions">
+          <button @click="togglePreview">
+            <span v-if="!showAllNotes">Show all</span>
+            <span v-else>Hide all</span>
+          </button>
+          <button v-if="!showAllNotes" @click="toggleNotes">
+            Edit notes
+          </button>
+        </div>
       </div>
     </div>
   </plant-component>
@@ -57,11 +73,15 @@
     data () {
       return {
         showNotes: false,
-        textContent: this.content
+        textContent: this.content,
+        showAllNotes: false
       }
     },
 
     methods: {
+      togglePreview () {
+        this.showAllNotes = !this.showAllNotes
+      },
       toggleNotes () {
         this.showNotes = !this.showNotes
       },
@@ -89,7 +109,6 @@
 <style lang="postcss" scoped>
   p {
     color: var(--text-color-secondary);
-    margin-bottom: var(--base-gap);
   }
 
   textarea {
@@ -99,5 +118,86 @@
     padding: var(--base-gap);
     background: var(--light-grey);
     border-radius: var(--border-radius);
+  }
+
+  .notes-available,
+  .notes-empty {
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+
+    &.show-all {
+      display: block;
+    }
+  }
+
+  .notes-empty {
+    & p {
+      width: 100%;
+      margin-right: var(--base-gap);
+      flex: 1;
+    }
+
+    button {
+      flex: 1;
+    }
+  }
+
+  .notes-preview {
+    background: var(--background-secondary);
+    border-radius: var(--border-radius);
+    padding: var(--base-gap);
+    font-style: italic;
+    width: 100%;
+    flex: 1;
+    margin-right: var(--base-gap);
+    position: relative;
+
+    @nest .show-all &::after {
+      display: none;
+    }
+
+    &::after {
+      display: block;
+      content: "";
+      width: 100%;
+      height: calc(3 * var(--base-gap));
+      background: linear-gradient(180deg, transparent, var(--background-secondary) 60%);
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      border-radius: var(--border-radius);
+    }
+
+    & p {
+      max-height: 16vh;
+      overflow: hidden;
+      width: 100%;
+    }
+
+    @nest .show-all & p::before {
+      display: block;
+      content: "";
+      width: 110px;
+      height: 55px;
+      float: right;
+      margin-top: calc(-1 * var(--base-gap));
+      margin-right: calc(-1 * var(--base-gap));
+    }
+  }
+
+  .notes-actions {
+    display: flex;
+    flex-direction: column;
+
+    @nest .show-all & {
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+
+    & button:first-of-type {
+      margin-bottom: var(--base-gap);
+    }
   }
 </style>
