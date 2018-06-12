07996d2f570b984a191b6a9ddc0031d8c33b949e
Moritz Kröger <write@morkro.de>
2018-06-12 22:49:26 +0200

    :bug: (overview) Reorder plants when data changes

--- src/app/overview/store/mutations.js
+++ src/app/overview/store/mutations.js
@@ -1,19 +1,23 @@
 import Vue from 'vue'
 import { sortByDate, sortByAlphabet } from '@/utils/sort'
 
+const sortPlants = (state) => {
+  switch (state.settings.orderBy) {
+    case 'latest':
+      state.plants = state.plants.sort(sortByDate).reverse()
+      break
+    case 'alphabetical':
+      state.plants = state.plants.sort(sortByAlphabet)
+      break
+  }
+}
+
 export default {
   UPDATE_VIEWMODE (state, payload) {
     state.updated = Date.now()
     state.settings = Object.assign({}, state.settings, payload)
 
-    switch (state.settings.orderBy) {
-      case 'latest':
-        state.plants = state.plants.sort(sortByDate).reverse()
-        break
-      case 'alphabetical':
-        state.plants = state.plants.sort(sortByAlphabet)
-        break
-    }
+    sortPlants(state)
   },
 
   UPDATE_PLANT_OVERVIEW (state, payload) {
@@ -23,6 +27,7 @@ export default {
 
     if (state.plants[itemIndex]) {
       Vue.set(state.plants, itemIndex, Object.assign(state.plants[itemIndex], payload.item))
+      sortPlants(state)
     }
   }
 }
