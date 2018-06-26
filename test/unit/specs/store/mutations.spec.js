b602549ef9d284f87fcbc03005cedb5e20e1104c
Moritz Kröger <write@morkro.de>
2018-06-26 21:21:36 +0200

    :white_check_mark: Updates Vuex mutations tests

--- test/unit/specs/store/mutations.spec.js
+++ test/unit/specs/store/mutations.spec.js
@@ -56,6 +56,87 @@ describe('store/mutations/settings.js', () => {
   })
 })
 
+describe('store/mutations/plants.js', () => {
+  // it('LOAD_PLANTS', () => {
+  //   const state = {
+  //     plants: []
+  //   }
+  //   mutations.LOAD_PLANTS(state, {})
+  //   expect(state).toBe()
+  // })
+  //
+  // it('LOAD_PLANT_ITEM', () => {
+  //   const state = {
+  //     plants: []
+  //   }
+  //   mutations.LOAD_PLANT_ITEM(state, {})
+  //   expect(state).toBe()
+  // })
+
+  it('ADD_PLANT', () => {
+    const then = new Date('2018-06-25').getTime()
+    const now = Date.now()
+    const state = {
+      plants: [{ name: 'Bar', created: then }],
+      settings: {
+        orderBy: 'alphabetical'
+      }
+    }
+
+    // Add and sort alphabetical
+    mutations.ADD_PLANT(state, { item: { name: 'Foo', created: now } })
+    expect(state.plants).toEqual(expect.arrayContaining([
+      { name: 'Bar', created: then },
+      { name: 'Foo', created: now }
+    ]))
+
+    // Add and sort by date (latest)
+    state.settings.orderBy = 'latest'
+    state.plants = [{ name: 'Bar', created: then }]
+    mutations.ADD_PLANT(state, { item: { name: 'Foo', created: now } })
+    expect(state.plants).toEqual(expect.arrayContaining([
+      { name: 'Foo', created: now },
+      { name: 'Bar', created: then }
+    ]))
+  })
+
+  it('DELETE_PLANTS', () => {
+    const state = {
+      plants: [
+        { guid: '526a9181-f84a-45c2-9a0a-7654979277c9' },
+        { guid: 'c05c08a7-552c-4895-a431-62455c6966df' },
+        { guid: '6e7e20bc-436e-4267-ae59-07e6686b6a2d' },
+        { guid: 'c0eb2d07-8504-40cf-ad63-efe3fc86fdbf' }
+      ]
+    }
+    mutations.DELETE_PLANTS(state, { items: [
+      { guid: 'c05c08a7-552c-4895-a431-62455c6966df' },
+      { guid: 'c0eb2d07-8504-40cf-ad63-efe3fc86fdbf' }
+    ]})
+    expect(state.plants).toEqual(expect.arrayContaining([
+      { guid: '526a9181-f84a-45c2-9a0a-7654979277c9' },
+      { guid: '6e7e20bc-436e-4267-ae59-07e6686b6a2d' }
+    ]))
+  })
+
+  it('UPDATE_PLANT', () => {
+    const state = {
+      plants: [{
+        name: 'Bar',
+        guid: '526a9181-f84a-45c2-9a0a-7654979277c9'
+      }]
+    }
+    mutations.UPDATE_PLANT(state, { data: {
+      name: 'New name',
+      guid: '526a9181-f84a-45c2-9a0a-7654979277c9'
+    } })
+    expect(state.plants).toEqual(expect.arrayContaining([{
+      name: 'New name',
+      guid: '526a9181-f84a-45c2-9a0a-7654979277c9'
+    }]))
+  })
+})
+
 describe('store/mutations/tags.js', () => {
   it('LOAD_TAGS', () => {
     const state = {
@@ -90,10 +171,51 @@ describe('store/mutations/tags.js', () => {
 
   it('DELETE_TAG', () => {
     const state = {
-      tags: []
+      tags: [
+        {
+          name: 'super-tag',
+          label: 'Super tag',
+          guid: '6e7e20bc-436e-4267-ae59-07e6686b6a2d',
+          plants: ['guid']
+        },
+        {
+          name: 'awesome-tag',
+          label: 'Awesome tag',
+          guid: 'c0eb2d07-8504-40cf-ad63-efe3fc86fdbf',
+          plants: ['guid', 'guid2']
+        }
+      ]
     }
-    mutations.DELETE_TAG(state)
-    expect()
+
+    // Force delete works as expected
+    mutations.DELETE_TAG(state, { item: {
+      tag: '6e7e20bc-436e-4267-ae59-07e6686b6a2d',
+      forceDelete: true
+    } })
+    expect(state.tags.length).toEqual(1)
+    expect(state.tags.find(t => t.guid === '6e7e20bc-436e-4267-ae59-07e6686b6a2d'))
+      .toBeUndefined()
+
+    // Delete just the plant
+    mutations.DELETE_TAG(state, { item: {
+      tag: 'c0eb2d07-8504-40cf-ad63-efe3fc86fdbf',
+      plant: 'guid'
+    } })
+    expect(state.tags).toEqual(expect.arrayContaining([{
+      name: 'awesome-tag',
+      label: 'Awesome tag',
+      guid: 'c0eb2d07-8504-40cf-ad63-efe3fc86fdbf',
+      plants: ['guid2']
+    }]))
+
+    // Delete tag if no plants anymore
+    mutations.DELETE_TAG(state, { item: {
+      tag: 'c0eb2d07-8504-40cf-ad63-efe3fc86fdbf',
+      plant: 'guid2'
+    } })
+    expect(state.tags.length).toEqual(0)
+    expect(state.tags.find(t => t.guid === '6e7e20bc-436e-4267-ae59-07e6686b6a2d'))
+      .toBeUndefined()
   })
 
   it('UPDATE_TAG', () => {
