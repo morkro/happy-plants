7876f5f7fb6117d59da4e9b91d266cac7795e412
Moritz Kröger <write@morkro.de>
2018-07-16 22:30:32 +0200

    :sparkles: (settings) Added changelog to both repository and app

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,19 @@ function resolve (dir) {
 }
 
 module.exports = {
+  chainWebpack: config => {
+    config.module.rule('md')
+      .test(/\.md/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-markdown-loader')
+      .loader('vue-markdown-loader/lib/markdown-compiler')
+      .options({
+        raw: true
+      })
+  },
+
   configureWebpack: config => {
     config.resolve.alias['#'] = resolve('')
   },
