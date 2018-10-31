f9ad5818e9b995d7d7d5f4872363b52d50db43df
Moritz Kröger <write@morkro.de>
2018-10-31 20:13:04 +0100

    :sparkles: Now using lazy loading for images in Overview

---
+++ src/components/LazyImage.vue
@@ -0,0 +1,64 @@
+<template>
+  <figure v-lazyload class="lazy-image">
+    <feather-loader class="rotate" />
+    <img
+      :data-src="source"
+      :alt="alt"
+      :title="title">
+  </figure>
+</template>
+
+<script>
+  export default {
+    name: 'LazyImage',
+
+    components: {
+      'feather-loader': () =>
+        import('vue-feather-icons/icons/LoaderIcon' /* webpackChunkName: "icons" */)
+    },
+
+    props: {
+      source: { type: String, required: true },
+      alt: { type: String, required: true },
+      title: { type: String, required: true }
+    }
+  }
+</script>
+
+<style lang="postcss">
+  @import "../styles/animations";
+
+  .lazy-image {
+    --preview-background: var(--grey);
+    width: 100%;
+    height: 100%;
+    position: relative;
+    background: var(--preview-background);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @nest html[data-theme="dark"] & {
+      --preview-background: var(--dark-grey);
+    }
+
+    &.loaded svg {
+      display: none;
+    }
+
+    & img {
+      object-fit: cover;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+
+    & svg {
+      width: 30%;
+      height: auto;
+      opacity: 0.12;
+      transform-origin: center center;
+      animation: rotate360 4s linear infinite;
+    }
+  }
+</style>
