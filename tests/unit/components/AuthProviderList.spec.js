1c1e6e47826f5a0a914ffab64f9c90acfb015270
Moritz Kröger <write@morkro.de>
2018-11-19 08:17:35 +0100

    :white_check_mark: Adding more tests

--- tests/unit/components/AuthProviderList.spec.js
+++ tests/unit/components/AuthProviderList.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import AuthProviderList from '@/components/AuthProviderList'
+import Button from '@/components/Button'
 
 const localVue = createLocalVue()
 
@@ -17,5 +18,15 @@ describe('components/AuthProviderList.vue', () => {
     const wrapper = shallowMount(AuthProviderList)
     expect(wrapper.props().loading).toEqual(false)
     expect(wrapper.props().disabled).toEqual(false)
+  })
+
+  it(`emit 'provider-selected' works`, () => {
+    const wrapper = shallowMount(AuthProviderList)
+    const index = 0
+    const button = wrapper.findAll(Button).at(index)
+    button.trigger('click')
+    expect(wrapper.emitted('provider-selected')).toBeTruthy()
+    expect(wrapper.emitted('provider-selected'))
+      .toEqual([[wrapper.vm.$data.signInOptions[index].key]])
   })
 })
--- tests/unit/not-found/NotFound.spec.js
+++ tests/unit/not-found/NotFound.spec.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import Router from 'vue-router'
 
 import NotFound from '@/app/not-found/NotFound'
+import Button from '@/components/Button'
 import store from '@/store'
 import router from '@/router'
 
@@ -20,5 +21,13 @@ describe('app/not-found/NotFound.vue', () => {
   it('is a Vue component', () => {
     const wrapper = shallowMount(NotFound, options)
     expect(wrapper.isVueInstance()).toEqual(true)
+  })
+
+  it('click to overview works', () => {
+    const wrapper = shallowMount(NotFound, options)
+    const button = wrapper.find(Button)
+    wrapper.setMethods({ pushToSafety: jest.fn() })
+    button.trigger('click')
+    expect(wrapper.vm.pushToSafety).toHaveBeenCalled()
   })
 })
