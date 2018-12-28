75fb564179db3c3b5a1fb19ae674818cdee161fb
Moritz Kröger <write@morkro.de>
2018-12-28 21:15:16 +0100

    :sparkles: (gallery) Deleting gallery items from Firebase now possible

--- src/app/gallery/store/actions/delete.js
+++ src/app/gallery/store/actions/delete.js
@@ -1,16 +1,40 @@
-import sleep from '@/utils/sleep'
-
 import {
   updateEntry as updateEntryLF
 } from '@/api/localforage'
 
+import {
+  deleteEntry as deleteEntryFire,
+  deleteFile
+} from '@/api/firebase'
+
 const namespace = 'gallery'
+const rootFolder = 'plants'
+const folder = 'gallery'
 
 export async function deleteGalleryItem ({ state, commit, dispatch }, data) {
   commit('DELETE_GALLERY_ITEM_PROGRESS')
 
   const updated = Date.now()
   await updateEntryLF('updated', updated)
+
+  if (state.storage.type === 'cloud') {
+    try {
+      const path = [
+        ['users', state.user.id],
+        [rootFolder, state.plants.selected.guid],
+        [folder, data.item.guid]
+      ]
+      await Promise.all([
+        await deleteEntryFire(path),
+        await deleteFile(path)
+      ])
+    } catch (error) {
+      commit('DELETE_GALLERY_ITEM_FAILURE')
+    }
+  } else {
+    await updateEntryLF(namespace, state.gallery.data)
+    console.warn('GALLERY SHOULD NOT BE STORED LOCALLY') // eslint-disable-line
+  }
 
   if (!state.storage.migrationMode) {
     commit('DELETE_GALLERY_ITEM_SUCCESS', data)
@@ -19,14 +43,5 @@ export async function deleteGalleryItem ({ state, commit, dispatch }, data) {
       item: data.item.guid,
       type: 'delete'
     })
-  }
-
-  if (state.storage.type === 'cloud') {
-    // delete from firebase
-    await sleep(2000)
-    console.warn('DELETE TO FIREBASE NOT IMPLEMENTED YET')
-  } else {
-    await updateEntryLF(namespace, state.gallery.data)
-    console.warn('GALLERY SHOULD NOT BE STORED LOCALLY')
   }
 }
