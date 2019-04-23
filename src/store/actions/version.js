a281e3b526862a18f8237eff69701f1a21ceec10
Moritz Kröger <write@morkro.de>
2019-04-23 07:49:20 +0200

    :bug: Fixes version updates

--- src/store/actions/version.js
+++ src/store/actions/version.js
@@ -2,14 +2,17 @@ import { getEntry, updateEntry } from '@/api/localforage'
 
 const namespace = 'version'
 
-export const updateVersion = async ({ state, commit }) => {
+export const loadVersion = async ({ commit }) => {
   const prevStorage = await getEntry(namespace)
   if (!prevStorage.version) {
     await commit('REGISTER_FIRST_TIME_USER')
     return
   }
 
-  await commit('LOAD_VERSION', prevStorage)
-  await updateEntry(namespace, state.version)
-  await commit('UPDATE_VERSION')
+  commit('LOAD_VERSION', prevStorage)
+}
+
+export const updateVersion = ({ state, commit }) => {
+  return updateEntry(namespace, state.version)
+    .then(() => commit('UPDATE_VERSION'))
 }
